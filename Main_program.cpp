8ecba4d263233613c366dacb0e6bd34fefaf5ee5
NamanG-upta <51494614+NamanG-upta@users.noreply.github.com>
2020-10-22 14:12:11 +0530

    Update Main_program.cpp

--- Main_program.cpp
+++ Main_program.cpp
@@ -27,7 +27,7 @@ int main() {
             cin>>regno>>Num_2>>age;
             parkingLot.park(regno,age);
         }
-        else if(command=="Leave"){
+        else if(Task=="Leave"){
             int slotNo;
             cin>>slotNo;
             parkingLot.leave(slotNo);
